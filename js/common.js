8488c0dd16195be904d587397aaed8cfe8d56c15
seredokhov <seredokhovp@mail.ru>
2017-12-01 17:24:43 +0200

    fix

--- js/common.js
+++ js/common.js
@@ -47,23 +47,42 @@ $(function(){
 	var videoBlock = $('.video_block');
 	//var link = $('.stop');
 
-	$(media).on('canplaythrough', function(){
-		videoBlock.fadeIn("slow");
-		media.play();
-	})
-
-
-
-	$(media).on('ended abort error pause', function(){
-		$(this).fadeOut("slow", function(){
-			videoBlock.remove();
+	if($(document).width() > 1200) {
+		$(media).on('canplaythrough', function(){
+			videoBlock.fadeIn("slow");
+			media.play();
+		})
+		$(media).on('ended abort error pause', function(){
+			$(this).fadeOut("slow", function(){
+				videoBlock.remove();
+			});
 		});
-	});
+	}
+	else {
+		videoBlock.remove();
+	}
 
-	/*
-	link.click(function(){
-		$('#video')[0].pause();
+
+//.menu_link
+
+})
+
+
+// Мобильное меню
+
+$(function(){
+	var link = $('.menu_link');
+	var menu = $('.main_nav');
+	var overlay = $('.overlay');
+
+	link.on('click', function(){
+		menu.add($(this)).toggleClass('open');
+		overlay.fadeToggle();
+		return false;
 	})
-	*/
+	overlay.on('click', function(){
+		$(this).fadeOut();
+		menu.add(link).removeClass('open');
+	})
 
 })
